44965ac24c78923aaf84aff83c99a4fd2cb2ba7b
mauryiitr-prog <mauryiitr@gmail.com>
2026-08-15 13:21:52 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,84 @@
+const products = {
+
+  akRadiant: [
+
+    {
+      name: "Mosquito Net Stick Holder",
+      image: "mosquito-net-holder.png",
+      description: "Heavy Duty No-Drill Bed Pole Holder for Mosquito Net | Pack of 2",
+      price: "₹XXX",
+      amazon: "#"
+    },
+
+    {
+      name: "AK Radiant Product 2",
+      image: "",
+      description: "Product description will be added here.",
+      price: "₹XXX",
+      amazon: "#"
+    },
+
+    {
+      name: "AK Radiant Product 3",
+      image: "",
+      description: "Product description will be added here.",
+      price: "₹XXX",
+      amazon: "#"
+    },
+
+    {
+      name: "AK Radiant Product 4",
+      image: "",
+      description: "Product description will be added here.",
+      price: "₹XXX",
+      amazon: "#"
+    }
+
+  ],
+
+
+  vermaurFarms: [
+
+    {
+      name: "Premium Dal Badi",
+      image: "Premium-Dal-Badi.png",
+      description: "Premium Dal Badi, 400g | Protein-Rich Lentil Nuggets | Authentic Homemade Taste",
+      price: "₹550 / 400g",
+      amazon: "#"
+    },
+
+    {
+      name: "Kala Namak Rice",
+      image: "kala-namak-rice.png",
+      description: "Traditional aromatic rice from Eastern UP.",
+      price: "₹380 / Kg",
+      amazon: "#"
+    },
+
+    {
+      name: "Jau + Chana Sattu",
+      image: "jau-chana-sattu.png",
+      description: "Jaljeera Flavour | Ready to Drink | 10 Sachets × 25g",
+      price: "₹230 / Pack",
+      amazon: "#"
+    },
+
+    {
+      name: "Sona Masoori Rice",
+      image: "sona-masoori-rice.png",
+      description: "Light, fluffy and aromatic rice.",
+      price: "₹149 / Kg",
+      amazon: "#"
+    },
+
+    {
+      name: "Doodh Malai Rice",
+      image: "doodh-malai-rice.png",
+      description: "Rich, creamy and premium taste.",
+      price: "₹179 / Kg",
+      amazon: "#"
+    }
+
+  ]
+
+};
